bcc3a82c68337783c55ddc9ed3c35390190cc3ee
Joyce Ho <joyce880120@gmail.com>
2023-09-06 14:11:47 -0400

    Change rectangle to shape in main

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,6 +1,6 @@
 import { newRenderer } from "./renderer.js"
 import { Rectangle } from "./shapes/rectangle.js";
 
-const rectangle = Rectangle(2, 3)
-const renderer = newRenderer(rectangle)
+const rect: Shape = Rectangle(2, 3)
+const renderer = newRenderer(rect)
 renderer.draw();
